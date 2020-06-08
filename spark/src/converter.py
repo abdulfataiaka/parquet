37d62a9ed09bf5d9c0cd3a93931af5ec7d755351
abdulfataiaka <abdulfataiaka@gmail.com>
2020-06-08 13:10:57 +0100

    activate pyspark

--- spark/src/converter.py
+++ spark/src/converter.py
@@ -1,23 +1,30 @@
 import shutil
 from glob import glob
+from pyspark.sql import SparkSession
 
 class Converter:
-  def __init__(spark, names, csvdir, destdir):
-    self.spark = spark
-    self.names = names
+  def __init__(tbnames, csvdir, destdir):
     self.csvdir = csvdir
+    self.tbnames = tbnames
     self.destdir = destdir
 
-  def convert(self, csvpath):
+  def convert(self, spark, tbname):
     tmppath = '/tmp/sparki'
-    df = self.spark.read.csv(csvpath, header=True)
+    csvpath = f"{self.csvdir}/{tbname}.csv"
+
+    df = spark.read.csv(csvpath, header=True)
     df.write.parquet(tmppath, mode='overwrite')
     filepath = glob(f"{tmppath}/*.parquet")[-1]
     shutil.move(filepath, self.destdir)
     shutil.rmtree(tmppath)
 
-  def perform(self):
-    for name in self.names:
-      print(f"\n[*] Executing converter on: {name}")
-      csvpath = f"{self.csvdir}/{name}.csv"
-      self.convert(csvpath)
+  def perform():
+    print("\n[*] Initializing a spark session\n")
+    spark = SparkSession.builder.appName("spark").getOrCreate()
+
+    for tbname in self.tbnames:
+      print(f"\n[*] Executing converter on: {tbname}")
+      self.convert(spark, tbname)
+
+    print("\n[*] Stopping the spark session\n")
+    spark.stop()
--- spark/src/main.py
+++ spark/src/main.py
@@ -1,12 +1,15 @@
 import sys
+from converter import Converter
 
-srcdir = '/tmp/exports'
-destdir = '/tmp/imports'
 tbnames = sys.argv[1:]
+
+csvdir = '/tmp/exports'
+destdir = '/tmp/imports'
 
 print(f"\n[*] Tables to import: {'<Empty>' if not len(tbnames) else ''}")
 for tbname in tbnames:
   print(f'     * {tbname}')
 print('')
 
-print('Start actual conversions')
+converter = Converter(tbnames, csvdir, destdir)
+converter.perform()
